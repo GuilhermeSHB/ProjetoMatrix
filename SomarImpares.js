8242c96cae7c2c5b23353c2c57c3e6c500f82504
GuilhermeSHB <41237456+GuilhermeSHB@users.noreply.github.com>
2018-07-25 20:04:44 -0300

    Add files via upload

---
+++ SomarImpares.js
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+Created using JS Bin
+http://jsbin.com
+
+Copyright (c) 2018 by GuilhermeSHB (http://jsbin.com/vepobon/1/edit)
+
+Released under the MIT license: http://jsbin.mit-license.org
+-->
+<meta name="robots" content="noindex">
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+
+<script id="jsbin-javascript">
+function SomarImpares(Numero){
+  var Contador , Soma=0;
+  if(typeof Numero ==='number'){
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+  
+  
+}
+
+
+
+  
+ 
+</script>
+</body>
+</html>
