641421382955431b78b4469b9bd75645bc4d1d98
GuilhermeSHB <41237456+GuilhermeSHB@users.noreply.github.com>
2018-07-26 15:16:22 -0300

    Add files via upload

---
+++ MultiplicarInteiros.js
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+Created using JS Bin
+http://jsbin.com
+
+Copyright (c) 2018 by GuilhermeSHB (http://jsbin.com/vepobon/1/edit)
+
+Released under the MIT license: http://jsbin.mit-license.org
+-->
+<meta name="robots" content="noindex">
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+
+<script id="jsbin-javascript">
+function MultiplicarInteiros(Numero){
+  var Contador , Multiplicador=1;
+  if(typeof Numero ==='number'){
+    
+    for(Contador = 1 ; Contador <= Numero ; Contador++){
+      
+      Multiplicador = Multiplicador * Contador;
+      
+      
+      
+    }
+  
+    
+    
+    
+    
+    
+    
+    
+    
+     }
+  return Multiplicador;
+  
+}
+
+console.log(MultiplicarInteiros(3))
+
+  
+ 
+</script>
+</body>
+</html>
